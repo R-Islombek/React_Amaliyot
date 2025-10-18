cc3856c7734c0b046c9ccfc83def6a1dce91b231
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-10-19 03:48:37 +0500

    counter yasadik

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const App = () => {
-  const [count, setCount] = React.useState(0)
+  const [count, setCount] =  useState(0)
   return (
     <div>
-            <h1>{count}</h1>
-            <input type="text" onChange={(e) => setCount(e.target.value)} />
+            <h1>Count : {count} </h1>
+           <button onClick={()=>setCount(count + 1)} >Increament</button>
+           <button onClick={()=>setCount(count - 1)} >Decreament</button>
+           <button onClick={()=>setCount(0)} >Reset</button>
     </div>
   )
 }
