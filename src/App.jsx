0218ebe36325ad211b44a3da581e0a901c65c858
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-10-19 03:39:31 +0500

    yangi sahifa qoshdim

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+          <h1>{count}</h1>
+          <button onClick={()=>setCount(count + 1 )} >Increment</button>
+          <button onClick={()=>setCount(count - 1)}  >Decrament</button>
+          <button onClick={()=>setCount(0)} >Reset</button>         
+    </div>
+  )
+}
+
+export default App
