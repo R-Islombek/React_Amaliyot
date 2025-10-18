bf55cbbfdcc2f8471c004ca072279f36edf29f17
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-10-19 04:09:31 +0500

    inputlar bilan ishlash

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  const [count, setCount] =  useState(0)
+  const [inputValue, setInputValue] = useState('');
+
+  const [isError, setIsError] = useState(false);
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    
+    setInputValue(value); 
+
+    if (value.length < 6) {
+      setIsError(true); 
+    } else {
+      setIsError(false);
+    }
+  };
+
   return (
     <div>
-            <h1>Count : {count} </h1>
-           <button onClick={()=>setCount(count + 1)} >Increament</button>
-           <button onClick={()=>setCount(count - 1)} >Decreament</button>
-           <button onClick={()=>setCount(0)} >Reset</button>
+      <input 
+        type="text" 
+        value={inputValue}
+        onChange={handleInputChange} 
+      />
+      {isError && <span>kamida 6 belgidan iborat bo'lishi kerak</span>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
