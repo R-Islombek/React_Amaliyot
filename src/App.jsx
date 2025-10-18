363730821964cd077f4ec1ace68244aac6c27d49
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-10-19 04:25:55 +0500

    O'xshadi

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
-  const [inputValue, setInputValue] = useState('');
 
-  const [isError, setIsError] = useState(false);
-  const handleInputChange = (e) => {
-    const value = e.target.value;
-    
-    setInputValue(value); 
+  const [x, setX] = useState(false);
 
-    if (value.length < 6) {
-      setIsError(true); 
-    } else {
-      setIsError(false);
-    }
-  };
+   const handleChange = (evt)=>{
+      if (evt.target.value.length  >= 6) {
+        setX(false);
+      } else if(evt.target.value.length  > 0){
+        setX(true);
+      }else{
+        setX(false);
+      }
+   }
+
+
 
   return (
     <div>
-      <input 
-        type="text" 
-        value={inputValue}
-        onChange={handleInputChange} 
-      />
-      {isError && <span>kamida 6 belgidan iborat bo'lishi kerak</span>}
+          <input type="text" onChange={handleChange} />
+          {x && <span>kamida 6 ta belgidan iborat bo'sin</span>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
